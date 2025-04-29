d7202b12a94e2fa21fb32f9cc1f439127398531a
IlyaKlagish <klagishia@gmail.com>
2025-04-30 00:13:00 +0300

    Урок 32:

--- pictures.py
+++ pictures.py
@@ -34,17 +34,20 @@ def move_enemy():
     enemy.goto(x, y)
 
 def move_enemy2():
-    x = player.xcor() - randint(40,100)
-    y = player.ycor() - randint(40,100)
+    x = player.xcor() - 74
+    y = player.ycor() - 74
+
     enemy2.goto(x, y)
 def game():
     move_enemy()
     move_enemy2()
     if player.distance(enemy) < 64:
+        player.home()
         player.write("Вы проиграли!", align="center",font=("Arial",24,"bold"))
         return
-    # screen.ontimer(game, 1000)
-    elif player.distance(enemy2) < 64:
+        screen.ontimer(game, 1000)
+    elif player.distance(enemy2) < 74:
+        player.home()
         player.write("Вы проиграли!", align="center",font=("Arial",24,"bold"))
         return
     screen.ontimer(game, 1000)
